7436a4133aafd1942b02476aeacd3b138acf2571
ayush <ayush738707@gmail.com>
2026-02-15 17:42:47 +0530

    Update API baseURL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    // baseURL: "http://localhost:5000/api"
+    baseURL: "https://real-state-backend-xj2z.onrender.com/api"
 })
 
-export default api;
+export default api
+
+// https://real-state-backend-xj2z.onrender.com
